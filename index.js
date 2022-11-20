bf8f984d2db4156180dff6628501d5312ae0af26
topce <topce@users.noreply.github.com>
2022-11-20 10:43:36 +0100

    add html form

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import * as wasm from "cirilica";
+import * as wasm from 'cirilica';
 
-alert(wasm.to_serbian_cyrillic("ana voli milovana"));
+document.title = wasm.to_serbian_cyrillic('ana voli milovana');
+
+document.getElementById('prepisi').onclick = function () {
+  myFunction();
+};
+
+function myFunction(ev) {
+  document.getElementById('cirilica').value = wasm.to_serbian_cyrillic(document.getElementById('latinica').value);
+  
+}
